aea708b41a8b81cc20d97b511da022b220c9f411
Davy Stevenson <dstevenson@esri.com>
2014-04-23 14:29:41 -0700

    Remove calls to Geotrigger Service from app

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class BenchmarkViewer < Angelo::Base
   include Angelo::Tilt::ERB
 
   before do
-    @gt = Geotrigger::Application.new YAML.load_file 'geotrigger.yml'
+#    @gt = Geotrigger::Application.new YAML.load_file 'geotrigger.yml'
   end
 
   get '/' do
@@ -15,8 +15,6 @@ class BenchmarkViewer < Angelo::Base
     @os = params["os"]
     @wifi = params["wifi"]
     @mode = [@os, @tag, @wifi].join('_')
-    #@trigger_list = @gt.post 'trigger/list', tags: [@tag], boundingBox: :geojson
-    @trigger_list = @gt.post 'trigger/list', tags: ['biking'], boundingBox: :geojson
     erb :index
   end
 
